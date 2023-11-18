576d367d87de11b145f9e951c68ea5d1f5adbbcf
Yuxiang Chen <yuc129@ucsd.edu>
2023-11-17 17:56:50 -0800

    A1

--- A1/assignment1.py
+++ A1/assignment1.py
@@ -1,127 +1,177 @@
-import gzip
 from collections import defaultdict
+import math
+from tqdm import tqdm
+import gzip
+import random
+import heapq
+
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import linear_model, metrics
+
+
+random.seed(42)
+
+"""
+userID
+gameID
+hours
+hours_transformed
+early_access
+text
+date
+"""
 
 
 def readJSON(path):
-    for l in gzip.open(path, "rt"):
+    f = gzip.open(path, "rt")
+    f.readline()
+    for l in f:
         d = eval(l)
-        u = d["userID"]
-        try:
-            g = d["gameID"]
-        except Exception as e:
-            g = None
-        yield u, g, d
+        yield d["userID"], d["gameID"], d["hours_transformed"]
+
 
 allHours = []
 for l in readJSON("train.json.gz"):
     allHours.append(l)
-
+random.shuffle(allHours)
 hoursTrain = allHours[:165000]
 hoursValid = allHours[165000:]
 
 user_game = defaultdict(set)
 game_user = defaultdict(set)
+user_hours = defaultdict(dict)
+game_hours = defaultdict(dict)
+
 for i in hoursTrain:
     user_game[i[0]].add(i[1])
     game_user[i[1]].add(i[0])
+    user_hours[i[0]][i[1]] = i[2]
+    game_hours[i[1]][i[0]] = i[2]
 
 
+allGame = set(game_user.keys())
+negHoursTrain = [
+    (i[0], random.sample(list(allGame - user_game[i[0]]), 1)[0]) for i in hoursTrain
+]
 
-### Time-played 
-betaU = {}
-betaI = {}
-r_ui = {}
-globalAverage = 0
-for u, g, d in hoursTrain:
-    betaU[u] = 0
-    betaI[g] = 0
-    r_ui[(u, g)] = d["hours_transformed"]
-    globalAverage += d["hours_transformed"]
-globalAverage /= len(hoursTrain)
+negHoursValid = [
+    (i[0], random.sample(list(allGame - user_game[i[0]]), 1)[0]) for i in hoursValid
+]
 
-alpha = globalAverage  # Could initialize anywhere, this is a guess
 
+### Time-played
+### baseline    4.988273760366987
+### now         3.0629636914492
+### grade       1.5/2
 def iterate(lamb, alpha, betaU, betaI):
-    temp = 0
-    for u, g, d in hoursTrain:
-        temp += r_ui[(u, g)] - (betaU[u] + betaI[g])
-    alpha = temp / len(hoursTrain)
+    new_betaU = {
+        u: sum([user_hours[u][g] - (alpha + betaI[g]) for g in user_game[u]]) / (lamb + len(user_game[u]))
+        for u in betaU
+    }
+    new_betaI = {
+        g: sum([user_hours[u][g] - (alpha + betaU[u]) for u in game_user[g]]) / (lamb + len(game_user[g]))
+        for g in betaI
+    }
+    a = sum(
+        [(user_hours[u][g] - (new_betaU[u] + new_betaI[g])) for u, g, _ in hoursTrain]
+    ) / len(hoursTrain)
+    return a, new_betaU, new_betaI
 
-    for u in betaU:
-        temp = 0
-        for g in user_game[u]:
-            temp += r_ui[(u, g)] - (alpha + betaI[g])
-        betaU[u] = temp / (lamb + len(user_game[u]))
 
-    for g in betaI:
-        temp = 0
-        for u in game_user[g]:
-            temp += r_ui[(u, g)] - (alpha + betaU[u])
-        betaI[g] = temp / (lamb + len(game_user[g]))
-    return alpha
-
+alpha = numpy.median([i[2] for i in hoursTrain])
+betaU = {u: 0 for u in user_game}
+betaI = {g: 0 for g in game_user}
 for i in range(10):
-    alpha = iterate(5, alpha, betaU, betaI)
+    alpha, betaU, betaI = iterate(5, alpha, betaU, betaI)
 
-predictions = open("predictions_Hours.csv", 'w')
-for l in open("pairs_Hours.csv"):
-    if l.startswith("userID"):
-        predictions.write(l)
-        continue
-    u,g = l.strip().split(',')
-    
-    pred = alpha + betaU[u] + betaI[g]
+preds = []
+labels = []
+for u, g, h in hoursValid:
+    preds.append(
+        alpha
+        + betaU.get(u, numpy.average([i for i in betaU.values()]))
+        + betaI.get(g, numpy.average([i for i in betaI.values()]))
+    )
+    labels.append(h)
+print(metrics.mean_squared_error(labels, preds))
 
-    _ = predictions.write(u + ',' + g + ',' + str(pred) + '\n')
+with open("predictions_Hours.csv", "w") as f:
+    for l in open("pairs_Hours.csv"):
+        if l.startswith("userID"):
+            f.write(l)
+            continue
+        u, g = l.strip().split(",")
 
-predictions.close()
+        # Logic
+        pred = (
+            alpha
+            + betaU.get(u, numpy.average([i for i in betaU.values()]))
+            + betaI.get(g, numpy.average([i for i in betaI.values()]))
+        )
+
+        _ = f.write(u + "," + g + "," + str(pred) + "\n")
+
 
 ### Would-play
-def jaccard_simularity(g1, g2):
-    u1 = game_user[g1]
-    u2 = game_user[g2]
-    num = len(u1 & u2)
-    den = len(u1 | u2)
+### baseline    0.6806
+### now         0.6929
+## grade        1/2
+medianUser = numpy.median([len(i) for i in user_game.values()])
+medianGame = numpy.median([len(i) for i in game_user.values()])
+
+
+def jaccard_simularity(s1, s2):
+    num = len(s1 & s2)
+    den = len(s1 | s2)
     return num / den if den > 0 else 0
 
 
-def jaccard_feat(u, g):
-    ans = 0
-    for g2 in user_game[u]:
+def topN_jaccard(u, g, N):
+    ans = [0 for _ in range(N)]
+    for g2 in user_game.get(u, set()):
         if g != g2:
-            ans = max(ans, jaccard_simularity(g, g2))
-    return ans
+            heapq.heappushpop(
+                ans,
+                jaccard_simularity(game_user.get(g, set()), game_user.get(g2, set())),
+            )
+    ans.sort(reverse=True)
+    return ans[:N]
 
 
-gameCount = defaultdict(int)
-allGame = set()
-for i in allHours:
-    gameCount[i[1]] += 1
-    allGame.add(i[1])
-totalPlayed = len(allHours)
-mostPopular = sorted([(gameCount[x], x) for x in gameCount], reverse=True)
-predictions = open("predictions_Played.csv", "w")
-for l in open("pairs_Played.csv"):
-    if l.startswith("userID"):
-        predictions.write(l)
-        continue
-    u, g = l.strip().split(",")
+def feat(d):
+    u, g = d[0], d[1]
+    user_pop = math.log(medianUser)
+    game_pop = math.log(medianGame)
+    if u in user_game:
+        user_pop = math.log(len(user_game[u]))
+    if g in game_user:
+        game_pop = math.log(len(game_user[g]))
+    return numpy.array([user_pop, game_pop] + topN_jaccard(u, g, 1))
 
-    count = 0
-    return1 = set()
-    limit = 0
-    for ic, i in mostPopular:
-        count += ic
-        return1.add(i)
-        limit = ic
-        if count > totalPlayed * 0.7:
-            break
-    feat = jaccard_feat(u, g)
-    if feat > 0.06:
-        pred = 1
-    elif g in return1 and feat > 0.015:
-        pred = 1
 
-    _ = predictions.write(u + "," + g + "," + str(pred) + "\n")
+clf = linear_model.LogisticRegression(class_weight="balanced")
+X = [feat(d) for d in tqdm(hoursTrain + negHoursTrain)]
+Y = [1 for _ in range(len(hoursTrain))] + [0 for _ in range(len(negHoursTrain))]
+clf.fit(X, Y)
 
-predictions.close()
+validX = [feat(d) for d in tqdm(hoursValid + negHoursValid)]
+validY = [1 for _ in range(len(hoursValid))] + [0 for _ in range(len(negHoursValid))]
+pred = clf.predict(validX)
+print(metrics.accuracy_score(validY, pred))
+
+with open("predictions_Played.csv", "w") as f:
+    testX = []
+    users = []
+    games = []
+    for l in open("pairs_Played.csv"):
+        if l.startswith("userID"):
+            f.write(l)
+            continue
+        u, g = l.strip().split(",")
+        users.append(u)
+        games.append(g)
+        testX.append(feat([u, g]))
+    # Logic
+    for u, g, pred in zip(users, games, clf.predict(testX)):
+        _ = f.write(u + "," + g + "," + str(pred) + "\n")
